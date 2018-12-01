aa9d085455f7fd29c9bfa4d9170e00afd9d92dde
sayopaul <yobizzi416@gmail.com>
2018-12-01 22:58:13 +0100

    everything is working fine now , just add the key words thing and fiam

--- index.php
+++ index.php
@@ -4,65 +4,71 @@ error_reporting(E_ALL);
     require "vendor/autoload.php";
     //the task is to check the homepage for three words . University , Federal Goverment and Student , Anytime we see any of these words , we click the article and then fetch the article's title , the article , the date ,the image URL ,the category
     use Goutte\Client;
-    $client = new Client();
-    $crawler = $client->request('GET','https://punchng.com/topics/news');
-    $articles = [];
-    $datePublished = [];
-    $categories = [];
-    try{
-    $crawler->filter('.seg-title')->each(function ($node) use(&$headings,$crawler,$client,&$articles,&$datePublished,&$categories){
-        $link = $crawler->filter("[title='".$node->text()."']")->attr('href');
-        $crawler = $client->request('GET',$link);
-        $date = $crawler->filter(".entry-date.published")->extract('_text');
-        $datePublished[] = $date;
-        date_default_timezone_set('Africa/Lagos');
-        $articleTimestamp = strtotime($date);
-        $articleDay = date('z',$articleTimestamp);
-        $currentDay = date('z'); 
-        print $articleDay . "<br>";
-        print $currentDay . "<br>";
-        if( $articleDay + 1 >= $currentDay  ){
-                $article = "";
-                $crawler->filter('.entry-content > p')->each(function($node)use(&$article,&$articles){
-                        // var_dump($node->text());
-                        $article .= $node->text();
-                        
-                });
-                $crawler->filter('.tags-links a:not([href])')->each(function($node) use(&$categories){
-                        $categories[] = $node->text();
-                        
-                });
-                
-                $image = $crawler->filter('.blurry')->eq(0)->attr('style');
-                $imageURL = strstr(strstr($image,"h"),"'",TRUE) . "<br><br>";
-                $articles[] = $article;
-                //for each article , do database insertion stuff here
-                function saveInDatabase($postTitle,$postBody,$postImageURL,$postDate,$postCategory){
+    /* function saveInDatabase($postTitle,$postBody,$postImageURL,$postDate,$postCategory){
 
-                }
-        }else{
-                //we do not want to continue and we break out .
+     } */
+    function scrapePunch($newsCategory){
+        $client = new Client();
+        $newsCategory = strtolower($newsCategory);
+        $i = 1;
+        while($i < 5 ){
+               /*  $punchPage = $i <= 1 ? 'https://punchng.com/topics/'.$newsCategory :'https://punchng.com/topics/'.$newsCategory.'/page/'.$i; */
+               $punchPage = "https://punchng.com/topics/news/page/3/";
+                $crawler = $client->request('GET',$punchPage);
+                $articles = [];
+                $datePublished = [];
+                $categories = [];
+                $j = 0;
+                $crawler->filter('.seg-title')->each(function ($node) use($crawler,$client,&$articles,&$datePublished,&$categories,$i,&$j){
+                        print $i ." " . $node->text() . "<br> <br>";
+                        // var_dump($node);
+                        // $link = $crawler->filter('div.items > a[title="'.$node->text().'"]')->extract('href');
+                        $links = $crawler->filter('.items.col-sm-12 > a')->extract('href');
+                        $link = $links[$j];
+                        print $j . "<br> <br>";
+                        var_dump($link);
+                        $crawler = $client->request('GET',$link);
+                        $date = $crawler->filter(".entry-date.published")->extract('_text');
+                        var_dump($date);
+                        $datePublished[] = $date[0];
+                        date_default_timezone_set('Africa/Lagos');
+                        $articleTimestamp = strtotime($date[0]);
+                        $articleDay = date('z',$articleTimestamp);
+                        $currentDay = date('z');
+                        // var_dump($articleDay); 
+                        if( $articleDay + 1 >= $currentDay  ){
+                                // echo "true";
+                                $article = "";
+                                $crawler->filter('.entry-content > p')->each(function($node)use(&$article,&$articles){
+                                        // var_dump($node->text());
+                                        $article .= $node->text();
+                                        
+                                });
+                                $crawler->filter('.tags-links a:not([href])')->each(function($node) use(&$categories){
+                                        $categories[] = $node->text();
+                                        // var_dump($categories);
+                                        
+                                });
+                                
+                                $image = $crawler->filter('.blurry')->eq(0)->attr('style');
+                                $imageURL = strstr(strstr($image,"h"),"'",TRUE) . "<br><br>";
+                                // var_dump($imageURL);
+                                $articles[] = $article;
+                                //for each article , do database insertion stuff here
+                        }else{
+                                //we do not want to continue and we break out .
+                                $i = 500;
+                        }
+                        
+                       
+                   $j++;     
+                });
+               $i++;
         }
-        
-    });
-    }catch(Exception $e){
-        var_dump($e);
-        echo " it is from here oo";
-    }
-        var_dump($datePublished);
+    } 
+    scrapePunch("news");
+    
+    
+       /*  var_dump($datePublished);
         var_dump($categories);
-        print_r($articles);
-//     $client = new Client();
-//     $crawler = $client->request('GET','https://punchng.com/topics/news');
-//     for($i=0; $i<count($headings); $i++){
-
-//         $crawler->filter("[title='".$headings[$i]."']")->attr('href')->each(function($node){
-//                 print $node->text();
-//         });
-
-        // $link = $crawler->selectLink($headings[$i])->link();
-        // $crawler = $client->click($link);
-        // $article = $crawler->filter('article');
-        // var_dump($article->text());
-//     }
-   
+        print_r($articles); */
